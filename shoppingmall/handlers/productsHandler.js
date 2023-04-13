77a7d1585d973315eb566ec08bf535c31e7d82f6
inzilion <inzilion@gmail.com>
2023-04-13 16:30:54 +0900

    2023-04-13 last

--- shoppingmall/handlers/productsHandler.js
+++ shoppingmall/handlers/productsHandler.js
@@ -1,7 +1,6 @@
 const pool = require('./_dbPool');
-
 const list = (req, res)=>{
-  let sql = `SELECT idproducts, name, price, registrationDate, seller From products`;
+  let sql = `SELECT idproducts, name, price, DATE_FORMAT(registrationDate, '%Y-%m-%d') AS date, seller From products`;
   pool.query(sql, (err, rows, field)=>{
     res.render('productsList.html', { products: rows});
   })  
